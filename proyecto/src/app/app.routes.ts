ecf6fa40ed9e2d6a777cc22baf534190d5e747d1
sonikal <alumno24.ruckj.tiago@ipm.edu.ar>
2024-08-19 12:24:12 -0300

    a

---
+++ proyecto/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { PerfilComponent } from './perfil/perfil.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { Top50Component } from './top50/top50.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { IngresoComponent } from './ingreso/ingreso.component';
+
+export const routes: Routes = [
+    { path: 'perfil/:id', component: PerfilComponent },
+    { path: '', component: InicioComponent },
+    { path: 'top50', component: Top50Component },
+    { path: 'reviews', component: ReviewsComponent },
+    { path: 'ingreso', component: IngresoComponent }
+];
